b618626672e4c8d2bc7f92df44b7c267d61fd087
nanannannana <rhaehfdl032037@gmail.com>
2023-01-15 22:12:28 +0900

    feat: react prac commit

--- reactjs/230113/src/App.js
+++ reactjs/230113/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import PracEvent1 from './components/PracEvent1';
+import Fragment from './components/Fragment';
 
 function App() {
   return (
     <div className="App">
-      <PracEvent1 />
+      <Fragment />
     </div>
   );
 }
---
+++ reactjs/230113/src/components/Fragment.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Fragment() {
+  return (
+    <>
+      <div>Hello!</div>
+    </>
+  );
+}
