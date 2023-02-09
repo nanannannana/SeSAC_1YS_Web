d215e246d29e80db41dc0763a82e74254f6c9f63
nanannannana <rhaehfdl032037@gmail.com>
2023-02-09 11:45:02 +0900

    feat: socket ui commit

--- nodejs/230209/index.js
+++ nodejs/230209/index.js
@@ -6,7 +6,8 @@ const io = require("socket.io")(http);
 app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 
 io.on("connection", (socket) => {
-  console.log("Server Socket Connected");
+  // console.log("Server Socket Connected", socket.id);
+  io.emit("notice", `${socket.id}님이 입장하였습니다.`);
 });
 
 http.listen(8000, () => console.log("server port: ", 8000));
