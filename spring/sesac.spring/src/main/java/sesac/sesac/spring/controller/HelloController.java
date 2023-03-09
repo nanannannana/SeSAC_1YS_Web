b67176df36745488f6f13d5a7d262432d738c32c
nanannannana <rhaehfdl032037@gmail.com>
2023-03-09 19:32:30 +0900

    feat: spring 수업

---
+++ spring/sesac.spring/src/main/java/sesac/sesac/spring/controller/HelloController.java
@@ -0,0 +1,58 @@
+package sesac.sesac.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hi") // = app.get
+    public String getHi(Model model) {
+        model.addAttribute("msg", "메세지입니다!");
+        model.addAttribute("utext", "<strong>utext입니다.</string>");
+        return "hi"; // = res.render("hi"), 리턴값 view파일로 인식
+    };
+
+    @GetMapping("/practice1")
+    public String getPractice(Model model) {
+        model.addAttribute("age", 20);
+        return "practice1";
+    };
+
+    public static class Person {
+        private int age;
+        private String name;
+
+        public Person(String name, int age) {
+            setAge(age);
+            setName(name);
+        }
+
+        public int getAge() {
+            return age;
+        }
+        public String getName() {
+            return name;
+        }
+        public void setAge(int age) {
+            this.age = age;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    @GetMapping("/people")
+    public String getPeople(Model model) {
+        ArrayList<Person> person = new ArrayList<>();
+        person.add(new Person("kim", 10));
+        person.add(new Person("seo", 20));
+        person.add(new Person("lee", 30));
+        person.add(new Person("park", 40));
+        model.addAttribute("person", person);
+        return "practice2";
+    }
+}
+
+
